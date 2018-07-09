5df57260d9deb9ab1028b301713326954f87968f
treycottingham <marchant.cottingham@gmail.com>
2018-07-09 09:38:40 -0600

    redeployed

--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -1,12 +1,25 @@
 import React from 'react'
 
 class InputForm extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      isShown: false
+    }
+  }
+  submitMessage = (e) => {
+    e.preventDefault()
+    this.setState({
+      isShown: true
+    })
+  }
   render() {
     return (
-      <form id="application-input">
+      <form onSubmit={this.submitMessage} id="application-input">
         <label>Apply Here: </label>
         <textarea onChange={event => this.props.setPreview(event.target.value)} id="application-text" rows="8" cols="100"></textarea>
         <input id="submit" type="submit" value="Submit" />
+        {this.state.isShown && <p>Your application was submitted!</p>}
       </form>
     )
   }
